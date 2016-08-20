c2de4dc1bda7beb30a0fa2e57800a936ac3d3f26
Ethan Macdonald <ethan.bryce.macdonald@gmail.com>
2016-08-20 19:02:18 -0400

    Add actor-critic test

---
+++ test_actor_critic.py
@@ -0,0 +1,78 @@
+from envs.mdp import StochasticMDPEnv
+import random
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+def actor():
+    actor = Sequential()
+    actor.add(Dense(6, init='lecun_uniform', input_shape=(6,)))
+    actor.add(Activation("relu"))
+    actor.add(Dense(2, init='lecun_uniform'))
+    actor.add(Activation("softmax"))
+    actor.compile(loss='mse', optimizer=Adam())
+    return actor
+
+def critic():
+    critic = Sequential()
+    critic.add(Dense(6, init='lecun_uniform', input_shape=(6,)))
+    critic.add(Activation("relu"))
+    critic.add(Dense(1, init='lecun_uniform'))
+    critic.compile(loss='mse', optimizer=Adam())
+    return critic
+
+class Agent:
+
+    def __init__(self):
+        self.actor = actor()
+        self.critic = critic()
+        self.epsilon = 0.1
+        self.gamma = 0.96
+
+    def select_move(self, state):
+        if self.epsilon < random.random():
+            return np.argmax(self.actor.predict(state, batch_size=32, verbose=0))
+        print("Epsilon!!")
+        return random.choice([0,1])
+
+    def eval(self, state):
+        return self.critic.predict(state, verbose=0)
+
+    def update(self, state, action, true_reward):
+        pred_reward = self.critic.predict(state)
+        td_error = true_reward - pred_reward
+        actor_reward = self.actor.predict(state)
+        actor_reward[0][action] = true_reward
+        self.critic.fit(state, true_reward, verbose=0)
+        self.actor.fit(state, actor_reward, verbose=0)
+
+def one_hot(state):
+    vector = np.zeros(6)
+    vector[state-1] = 1.0
+    return np.expand_dims(vector, axis=0)
+
+def main():
+    env = StochasticMDPEnv()
+    agent = Agent()
+    total_reward = np.zeros(100)
+    for episode in range(1000):
+        state = env.reset()
+        action = agent.select_move(one_hot(state))
+        print("State: %d, Action: %d" % (state, action))
+        state, reward, done = env.step(action)
+        while not done:
+            action = agent.select_move(one_hot(state))
+            print("State: %d, Action: %d" % (state, action))
+            next_state, reward, done = env.step(action)
+            agent.update(one_hot(state), action, reward + agent.gamma * agent.eval(one_hot(next_state)))
+            state = next_state
+        total_reward[episode % 100] = reward
+        print("Episode %d: DONE" % episode)
+        print(total_reward.mean())
+
+if __name__ == "__main__":
+    main()
