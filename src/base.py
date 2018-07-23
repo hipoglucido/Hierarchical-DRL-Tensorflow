ef4f5762d50afae90ff78e9c83234d6b82b3c735
hipoglucido <gcvictorgc@gmail.com>
2018-07-23 09:34:25 +0200

    Removed recording last steps because no DPI in server

--- src/base.py
+++ src/base.py
@@ -220,6 +220,7 @@ class Agent(object):
                     value = int(self.total_steps - self.step < record_last_n_steps)
             except:
                 value = 0
+        value = 0 #Can't record because dpi is needed
         self.config.gl.update({'display_prob' : value})        
         self.display_episode = random.random() < self.gl.display_prob   
         
